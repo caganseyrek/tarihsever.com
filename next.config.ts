9f438f5126042c20f9537de80c01f897027c86cb
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-04-01 19:42:59 +0300

    Removed rehype plugins

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeExternalLinks from "rehype-external-links";
-import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
@@ -13,11 +10,6 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "wrap" }],
-      [rehypeExternalLinks, { target: "_blank" }],
-    ],
   },
 });
 
