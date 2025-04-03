19df54182306361b62562405f91186635de2a2bf
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-04-03 19:05:44 +0300

    Generated files data files

--- prepublish/generated/shortlinks.ts
+++ prepublish/generated/shortlinks.ts
@@ -3,11 +3,71 @@ import { ShortLinkProps } from "@/prepublish/tasks/generate-shortlinks";
 
 export const shortLinks: ShortLinkProps[] = [
   {
-    shortLinkCode: "1Shne25M",
-    redirectsTo: "konular/koloniyalizm/afrikada-kolonicilik/cu",
+    shortLinkCode: "dGMoc1LT",
+    redirectsTo: "konular/koloniyalizm/afrikada-kolonicilik/example",
   },
   {
-    shortLinkCode: "drhyKeYy",
-    redirectsTo: "konular/koloniyalizm/afrikada-kolonicilik/cu",
+    shortLinkCode: "WWewsdGA",
+    redirectsTo: "konular/koloniyalizm/afrikada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "IfZ5EGjT",
+    redirectsTo: "konular/koloniyalizm/amerikada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "P7KyEsAG",
+    redirectsTo: "konular/koloniyalizm/asyada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "bqK2nf4J",
+    redirectsTo: "konular/koloniyalizm/dekolonizasyon/example",
+  },
+  {
+    shortLinkCode: "3dqH9aRJ",
+    redirectsTo: "konular/koloniyalizm/direnisler-ve-isyanlar/example",
+  },
+  {
+    shortLinkCode: "EAgqHxgq",
+    redirectsTo: "konular/koloniyalizm/imparatorluklar/example",
+  },
+  {
+    shortLinkCode: "UoIVMsSz",
+    redirectsTo: "konular/koloniyalizm/kesif-cagi/example",
+  },
+  {
+    shortLinkCode: "TqIL3WWN",
+    redirectsTo: "konular/koloniyalizm/pasifikte-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "W82KCbKq",
+    redirectsTo: "konular/koloniyalizm/afrikada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "MEs30XCv",
+    redirectsTo: "konular/koloniyalizm/amerikada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "DC3cw9Js",
+    redirectsTo: "konular/koloniyalizm/asyada-kolonicilik/example",
+  },
+  {
+    shortLinkCode: "K9v3kI0e",
+    redirectsTo: "konular/koloniyalizm/dekolonizasyon/example",
+  },
+  {
+    shortLinkCode: "qv1fIOe2",
+    redirectsTo: "konular/koloniyalizm/direnisler-ve-isyanlar/example",
+  },
+  {
+    shortLinkCode: "pweX9Fny",
+    redirectsTo: "konular/koloniyalizm/imparatorluklar/example",
+  },
+  {
+    shortLinkCode: "oWSdtqYw",
+    redirectsTo: "konular/koloniyalizm/kesif-cagi/example",
+  },
+  {
+    shortLinkCode: "p1fCLEun",
+    redirectsTo: "konular/koloniyalizm/pasifikte-kolonicilik/example",
   },
 ];
